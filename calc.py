16df6e03ee93274a8a2486093776e914de357dd4
Dreuid <lacanaria.lorenz@gmail.com>
2026-01-18 21:40:52 +0800

    multiply

--- calc.py
+++ calc.py
@@ -2,6 +2,13 @@ def add(x, y):
     """Returns the sum of x and y."""
     return x + y
 
+def subtract(x, y):
+    """Returns the difference of x and y."""
+    return x - y
+
+def multiply(x, y):
+    """Returns the product of x and y."""
+    return x * y
 
 def main():
     print("--- Python Calculator ---")
